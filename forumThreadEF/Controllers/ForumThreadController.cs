7a8d3b2d1d332e41c4bfdbab1de21fae960441f7
Pawel <piko333@wp.pl>
2022-10-31 12:25:45 +0100

    Add project files.

---
+++ forumThreadEF/Controllers/ForumThreadController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace forumThreadEF.Controllers
+{
+    [ApiController]
+    [Route("api/forumThread")]
+    public class ForumThreadController : ControllerBase
+    {
+        private readonly ILogger<ForumThreadController> _logger;
+
+        public ForumThreadController(ILogger<ForumThreadController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("abc")]
+        public ActionResult<int> Get()
+        {
+            return Ok(0);
+        }
+    }
+}
