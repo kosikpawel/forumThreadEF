4d9efff2bd8da86b03ccdbfdc317694638af829c
Pawel <piko333@wp.pl>
2022-11-03 22:58:44 +0100

    Create Database

---
+++ forumThreadEF/Entities/ForumThreadDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace forumThreadEF.Entities
+{
+    public class ForumThreadDbContext : DbContext
+    {
+        public ForumThreadDbContext(DbContextOptions<ForumThreadDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Vote> Votes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(this.GetType().Assembly);
+        }
+
+    }
+}
---
+++ forumThreadEF/Entities/Vote.cs
@@ -0,0 +1,12 @@
+﻿namespace forumThreadEF.Entities
+{
+    public class Vote
+    {
+        public int Value { get; set; }
+
+        public User User { get; set; }
+        public int UserId { get; set; }
+        public Post Post { get; set; }
+        public int PostId { get; set; }
+    }
+}
